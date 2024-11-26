0fbf795d891a1a5f28fd6e891f1b9db9c651d9be
heba <hebasaadabd03@gmail.com>
2024-11-27 00:52:15 +0200

    init

---
+++ resources/views/user_view/layout/nav_bar.blade.php
@@ -0,0 +1,104 @@
+<header>
+    <!-- Header desktop -->
+    <div class="container-menu-desktop">
+        <!-- Topbar -->
+        <div class="top-bar">
+            <div class="content-topbar flex-sb-m h-full container">
+                <div class="left-top-bar">
+                    Free shipping for standard order over $100
+                </div>
+
+                <div class="right-top-bar flex-w h-full">
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        Help & FAQs
+                    </a>
+
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        My Account
+                    </a>
+
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        EN
+                    </a>
+
+                    <a href="#" class="flex-c-m trans-04 p-lr-25">
+                        USD
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="wrap-menu-desktop">
+            <nav class="limiter-menu-desktop container">
+
+                <!-- Logo desktop -->
+                <a href="{{route('home')}}" class="logo">
+                    <img src="images/icons/logo-01.png" alt="IMG-LOGO">
+                </a>
+
+                <!-- Menu desktop -->
+                <div class="menu-desktop">
+                    <ul class="main-menu">
+                        <li class="active-menu">
+                            <a href="{{route('home')}}">Home</a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('product')}}">Shop</a>
+                        </li>
+
+                        <li class="label1" data-label1="hot">
+                            <a href="{{route('shoping_cart')}}">Features</a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('blog')}}">Blog</a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('about')}}">About</a>
+                        </li>
+
+                        <li>
+                            <a href="{{route('contact')}}">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Icon header -->
+                <div class="wrap-icon-header flex-w flex-r-m">
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+                        <i class="zmdi zmdi-search"></i>
+                    </div>
+
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+                        <i class="zmdi zmdi-shopping-cart"></i>
+                    </div>
+
+                    <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11" >
+                        <i class="zmdi zmdi-favorite-outline"></i>
+                    </a>
+
+                    <a href="{{route('register')}}" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 " >
+                        <i class="zmdi zmdi-account-circle"></i>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Modal Search -->
+    <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+        <div class="container-search-header">
+            <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+                <img src="images/icons/icon-close2.png" alt="CLOSE">
+            </button>
+
+            <form class="wrap-search-header flex-w p-l-15">
+                <button class="flex-c-m trans-04">
+                    <i class="zmdi zmdi-search"></i>
+                </button>
+                <input class="plh3" type="text" name="search" placeholder="Search...">
+            </form>
+        </div>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,32 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('user_view.home');
+})->name('home');
+
+Route::get('/product', function () {
+    return view('user_view.product.product');
+})->name('product');
+
+Route::get('/contact', function () {
+    return view('user_view.shared.contact');
+})->name('contact');
+
+Route::get('/about', function () {
+    return view('user_view.shared.about');
+})->name('about');
+
+Route::get('/blog', function () {
+    return view('user_view.shared.blog');
+})->name('blog');
+
+Route::get('/product_details', function () {
+    return view('user_view.product.product_details');
+})->name('product_details');
+
+Route::get('/shoping_cart', function () {
+    return view('user_view.cart.shoping_cart');
+})->name('shoping_cart');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
