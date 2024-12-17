a0323ecce6f754b6e757db2309d0370cdaaf3457
Omar <Omar Abdelgilil007>
2024-12-17 16:32:54 +0200

    login register template changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,42 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
+
+<x-auth-session-status class="mb-4" :status="session('status')" />
+<!-- Coding By CodingNepal - www.codingnepalweb.com -->
+
+<head>
+
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+<body>
+  <div class="wrapper">
     <form method="POST" action="{{ route('login') }}">
         @csrf
+      <h2>Login</h2>
+        <div class="input-field">
+        <input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+        <label for="email" :value="__('Email')" >Enter your email</label>
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+      </div>
+      <div class="input-field">
+        <input id="password"
+        type="password"
+        name="password"
+        required autocomplete="current-password" >
+        <label for="password" :value="__('Password')" >Enter your password</label>
+        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+      </div>
+      <div class="forget">
+        <label for="remember">
+          <input name="remember" id="remember_me" type="checkbox">
+          <p>Remember me</p>
+        </label>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a href="{{ route('register') }}" class=" text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 dark:focus:ring-offset-gray-800">
-                Register &emsp;
-            </a>
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+      </div>
+      <button type="submit">Log In</button>
+      <div class="register">
+        <p>Don't have an account? <a  href="{{ route('register') }}">Register</a></p>
+      </div>
     </form>
-</x-guest-layout>
+  </div>
+</body>
+
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,83 +1,72 @@
-<x-guest-layout>
+
+
+
+
+<head>
+
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+<body>
+  <div class="wrapper">
     <form method="POST" action="{{ route('register') }}">
         @csrf
+      <h2>Register</h2>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+      <div class="input-field">
+        <input id="name" type="text" name="name" :value="old('name')" required autofocus>
+        <label for="name">Enter your name</label>
+        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+      </div>
+
+      <div class="input-field">
+        <input id="email" type="email" name="email" :value="old('email')" required>
+        <label for="email">Enter your email</label>
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+      </div>
+
+      <div class="input-field">
+        <input id="phone" type="text" name="phone" :value="old('phone')" required>
+        <label for="phone">Enter your phone</label>
+        <x-input-error :messages="$errors->get('phone')" class="mt-2" />
+      </div>
+
+      <div class="input-field">
+        <input id="address" type="text" name="address" :value="old('address')" required>
+        <label for="address">Enter your address</label>
+        <x-input-error :messages="$errors->get('address')" class="mt-2" />
+      </div>
+
+      <div class="mt-4">
+        <x-input-label style="color: white" for="gender" :value="__('Gender')" />
+        <div class="flex items-center">
+            <label for="male" class="mr-4">
+                <input type="radio" id="male" name="gender" value="male" class="mr-2" :checked="old('gender') == 'male'" required>
+                Male &emsp;
+            </label>
+            <label for="female" class="mr-4">
+                <input type="radio" id="female" name="gender" value="female" class="mr-2" :checked="old('gender') == 'female'" required>
+                Female
+            </label>
         </div>
+        <x-input-error :messages="$errors->get('gender')" class="mt-2" />
+    </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+      <div class="input-field">
+        <input id="password" type="password" name="password" required>
+        <label for="password">Enter your password</label>
+        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+      </div>
 
-         <!-- phone -->
-         <div class="mt-4">
-              <x-input-label for="phone" :value="__('Phone')" />
-              <x-text-input id="phone" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autocomplete="username" />
-              <x-input-error :messages="$errors->get('phone')" class="mt-2" />
-         </div>
+      <div class="input-field">
+        <input id="password_confirmation" type="password" name="password_confirmation" required>
+        <label for="password_confirmation">Confirm your password</label>
+      </div>
 
-         <!-- address -->
-         <div class="mt-4">
-               <x-input-label for="address" :value="__('Address')" />
-               <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autocomplete="username" />
-               <x-input-error :messages="$errors->get('address')" class="mt-2" />
-          </div>
-
-          <!-- Gender -->
-          <div class="mt-4">
-            <x-input-label for="gender" :value="__('Gender')" />
-            <div class="flex items-center">
-                <label for="male" class="mr-4">
-                    <input type="radio" id="male" name="gender" value="male" class="mr-2" :checked="old('gender') == 'male'" required>
-                    Male &emsp;
-                </label>
-                <label for="female" class="mr-4">
-                    <input type="radio" id="female" name="gender" value="female" class="mr-2" :checked="old('gender') == 'female'" required>
-                    Female
-                </label>
-            </div>
-            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
-        </div>
-
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
+      <button type="submit">Register</button>
+      <div class="register">
+        <p>Already have an account? <a href="{{ route('login') }}">Log In</a></p>
+      </div>
     </form>
-</x-guest-layout>
+  </div>
+</body>
+
